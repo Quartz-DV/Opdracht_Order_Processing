06b49e355da9f5d3b44b55c1a7d6194d6ecf5252
bascman <198905876+bascman@users.noreply.github.com>
2026-03-24 11:16:50 +0100

    AAAAAA

---
+++ Order_Processing/OrderBL/Bestellingsklassen/Gold.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderBL.StatusKlassen
+{
+    public class Gold
+    {
+        private int _goldPrijs;
+        public int GoldPrijs
+        {
+            get { return _goldPrijs; }
+            set { _goldPrijs = 120 * 3; }
+        }
+    }
+}
