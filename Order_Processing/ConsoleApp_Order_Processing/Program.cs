eb3bc2b0835285b973c3f710334dd27d37a16c2c
Quartz-DV <238630229+Quartz-DV@users.noreply.github.com>
2026-03-27 08:32:06 +0100

    Changes in consoleAPp

--- Order_Processing/ConsoleApp_Order_Processing/Program.cs
+++ Order_Processing/ConsoleApp_Order_Processing/Program.cs
@@ -18,34 +18,28 @@ IBestellingRepository Bestellingrepo = RepositoryFactory.GeefBestellingRepositor
 OrderBeheerder Beheerder = new OrderBeheerder(Lidrepo,Eventrepo,Bestellingrepo);
 
 
-// 1. Eerst maak je de beheerder aan (Dit hadden we al)
-OrderBeheerder beheerder = new OrderBeheerder(lidRepo, eventRepo, bestellingRepo);
 
-// --- TESTDATA AANMAKEN (DATA SEEDING) ---
 
-// 2. Je verzint zelf een event en vult de prijs in
+
+
 Event event1 = new Event();
 event1.Naam = "Tech Conferentie 2026";
-event1.KostPrijs = 75.0m; // Hier stel je de basisprijs in!
+event1.KostPrijs = 75.0m;
 event1.Datum = new DateTime(2026, 6, 15);
 event1.Adres = "Expo Gent";
 
 Event event2 = new Event();
 event2.Naam = "VIP Zomerfestival";
-event2.KostPrijs = 150.0m; // Dit event is duurder!
+event2.KostPrijs = 150.0m; 
 event2.Datum = new DateTime(2026, 8, 20);
 event2.Adres = "Sportpaleis Antwerpen";
 
-// 3. Je bewaart ze in jouw database (collectie) via de beheerder
-beheerder.MaakEventAan(event1);
-beheerder.MaakEventAan(event2);
+//Beheerder.MaakEventAan(event1);
+//Beheerder.MaakEventAan(event2);
 
-// Ook handig: Maak alvast een test-lid aan
+
 Lid testLid = new Lid();
 testLid.Naam = "Sarah Peeters";
 testLid.Status = "Gold";
-beheerder.(testLid);
+Beheerder.(testLid);
 
-// --- EINDE TESTDATA ---
-
-// Vanaf hier begint je echte programma (UI tonen, menuutje printen, etc.)
