ea997a3dbb540bc8cc7cbedff365b3004927c5d4
ikepik3 <97116467+Ikepik3@users.noreply.github.com>
2026-03-31 10:22:57 +0200

    change MainWindow.XAML.cs

--- Order_Processing/Wpf_Order_Processing/MainWindow.xaml.cs
+++ Order_Processing/Wpf_Order_Processing/MainWindow.xaml.cs
@@ -59,9 +59,17 @@ namespace Wpf_Order_Processing {
                     
                     return;
                 }
-                else if (lid.Email == txtBoxEmail.Text && (bool)adminBox.IsChecked && lid.Admin == true)
+                else if (lid.Email == txtBoxEmail.Text && adminBox.IsChecked == true)
                 {
-                    adminWindow.Show();
+                    if (lid.Admin == true)
+                    {
+                        adminWindow.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("ACCESS DENIED");
+                    }
+                    
                 }
                 else {
 
